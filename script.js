b61adbe9454c092d83cde91fa0651da30542aae2
Jake Tyler <jrt@byu.edu>
2018-02-12 02:04:48 -0700

    full pokedex functionality

--- script.js
+++ script.js
@@ -1,105 +1,185 @@
 $(document).ready(function () {
 
-  $("#pokedexSubmit").click(function(e) {
+  $("#pokemonNameSubmit").click(function(e) {
     $("#pokedexEntries").empty();
     e.preventDefault();
     var pokemonName = $("#pokemonName").val();
+    document.getElementById("pokedexEntries").style.display = "none";
+    if (pokemonName != "") {
+      document.getElementById("ajax-loader").style.display = "inline";
+      getPokemonEntry(pokemonName);
+    } else {
+      alert("Please enter a name.");
+    }
+  });
+
+  $("#pokemonIdSubmit").click(function(e) {
+    $("#pokedexEntries").empty();
+    e.preventDefault();
+    var pokemonId = $("#onlyId").val();
+    document.getElementById("pokedexEntries").style.display = "none";
+    if (pokemonId == "") {
+      alert("Please enter an ID.");
+    } else if (!$.isNumeric(pokemonId)) {
+      alert("The input must be numeric.")
+    } else {
+      document.getElementById("ajax-loader").style.display = "inline";
+      getPokemonEntry(pokemonId);
+    }
+  });
+
+  $("#pokemonGroupSubmit").click(function(e) {
+    $("#pokedexEntries").empty();
+    e.preventDefault();
     var lowerValue = $("#lowerId").val();
     var upperValue = $("#upperId").val();
     document.getElementById("pokedexEntries").style.display = "none";
-    if (pokemonName != "") {
-      document.getElementById("ajax-loader").style.display = "block";
-      getPokemonEntryByName(pokemonName);
-    } else if (lowerValue != "" && upperValue != "") {
+    if (lowerValue == "" || upperValue == "") {
+      alert("Please enter two IDs.");
+    } else if (!$.isNumeric(lowerValue) || !$.isNumeric(upperValue)) {
+      alert("The input must be numeric.")
+    } else if (upperValue < lowerValue) {
+      alert("The range must go from one ID to an ID that is equal or higher.")
+    } else {
       document.getElementById("progressBar").style.display = "block";
       document.getElementById("loadedBar").style.width = "1%";
-      getPokemonEntryById(lowerValue, 0, upperValue-lowerValue+1);
-    } else {
-      alert("NO");
+      getPokemonEntries(lowerValue, 0, upperValue-lowerValue+1);
     }
   });
 });
 
-function getPokemonEntryByName(name) {
+function getPokemonEntry(name) {
   var pokedexEntries = document.getElementById("pokedexEntries");
-  var myurl= "https://pokeapi.co/api/v2/pokemon/" + name;
-  $.ajax({
-    url : myurl,
-    dataType : "json",
-    success : function(json) {
-      console.log(json);
-      var entry = document.createElement("div");
-      entry.className = "entry";
-
-      var sprite = document.createElement("img");
-      sprite.src = json.sprites.front_default;
-
-      var idNum = document.createElement("p");
-      idNum.innerHTML = json.id;
-
-      entry.appendChild(sprite);
-      entry.appendChild(idNum);
-
-      pokedexEntries.appendChild(entry);
-
-      pokedexEntries.style.display = "block";
-      document.getElementById("ajax-loader").style.display = "none";
-    }
-  });
-}
-
-function getPokemonEntryById(id, howManySoFar, howManyTotal) {
-  var pokedexEntries = document.getElementById("pokedexEntries");
-  var pokemonUrl = "https://pokeapi.co/api/v2/pokemon/" + id;
-  var speciesUrl = "https://pokeapi.co/api/v2/pokemon-species/" + id;
+  var pokemonUrl = "https://pokeapi.co/api/v2/pokemon/" + name + "/";
+  var speciesUrl = "https://pokeapi.co/api/v2/pokemon-species/" + name + "/";
   $.ajax({
     url : pokemonUrl,
     dataType : "json",
     success : function(pokemonJson) {
       console.log(pokemonJson);
       $.ajax({
-        url : pokemonUrl,
+        url : speciesUrl,
         dataType : "json",
         success : function(speciesJson) {
-          var entry = document.createElement("div");
-          entry.className = "entry";
+          console.log(speciesJson);
 
-          var sprite = document.createElement("img");
-          sprite.src = pokemonJson.sprites.front_default;
+          createEntry(pokemonJson, speciesJson);
 
-          var idNum = document.createElement("p");
-          idNum.innerHTML = id;
+          pokedexEntries.style.display = "block";
+          document.getElementById("ajax-loader").style.display = "none";
+        }
+      });
+    }
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    pokedexEntries.innerHTML = "<em>No Pokémon found.</em>"
+    pokedexEntries.style.display = "block";
+    document.getElementById("ajax-loader").style.display = "none";
+  });
+}
 
-          entry.appendChild(sprite);
-          entry.appendChild(idNum);
+function getPokemonEntries(id, howManySoFar, howManyTotal) {
+  var pokedexEntries = document.getElementById("pokedexEntries");
+  var pokemonUrl = "https://pokeapi.co/api/v2/pokemon/" + id + "/";
+  var speciesUrl = "https://pokeapi.co/api/v2/pokemon-species/" + id + "/";
+  $.ajax({
+    url : pokemonUrl,
+    dataType : "json",
+    success : function(pokemonJson) {
+      console.log(pokemonJson);
+      $.ajax({
+        url : speciesUrl,
+        dataType : "json",
+        success : function(speciesJson) {
+          console.log(speciesJson);
 
-          pokedexEntries.appendChild(entry);
+          createEntry(pokemonJson, speciesJson);
 
           ++howManySoFar;
           if (howManySoFar < howManyTotal) {
             var percent = 100*howManySoFar/(howManyTotal-1);
-            document.getElementById("myBar").style.width = percent + '%';
-            getPokemonEntryById(++id, howManySoFar, howManyTotal);
+            document.getElementById("loadedBar").style.width = percent + '%';
+            getPokemonEntries(++id, howManySoFar, howManyTotal);
           } else {
             pokedexEntries.style.display = "block";
-            document.getElementById("myProgress").style.display = "none";
-          }  
+            document.getElementById("progressBar").style.display = "none";
+          }
         }
       });
     }
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    pokedexEntries.innerHTML = "<em>No Pokémon found.</em>"
+    pokedexEntries.style.display = "block";
+    document.getElementById("progressBar").style.display = "none";
   });
 }
 
-/*function move() {
-  document.getElementById("myBar").style.width = width + '%';
-  var width = 1;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width++;
+function createEntry(pokemonJson, speciesJson) {
+  var entry = document.createElement("div");
+  entry.className = "entry";
 
+  var imageDiv = document.createElement("div");
+  imageDiv.className = "pokemonImage";
+  var helperSpan = document.createElement("span");
+  helperSpan.className = "helper";
+  imageDiv.appendChild(helperSpan);
+  var sprite = document.createElement("img");
+  sprite.src = pokemonJson.sprites.front_default;
+  imageDiv.appendChild(sprite);
+  entry.appendChild(imageDiv);
+
+  var dataDiv = document.createElement("div");
+  dataDiv.className = "pokemonData";
+  var dataHeader = document.createElement("h3");
+  dataHeader.innerHTML = titleCase(speciesJson.name) + " - #" + speciesJson.id;
+  dataDiv.appendChild(dataHeader);
+
+  var genera = speciesJson.genera;
+  var genus;
+  var i = 0;
+  while (true) {
+    if (genera[i].language.name == "en") {
+      genus = genera[i].genus;
+      break;
     }
+    i++;
   }
-}*/
+  var genusElement = document.createElement("h4");
+  genusElement.innerHTML = genus;
+  dataDiv.appendChild(genusElement);
+
+  var flavors = speciesJson.flavor_text_entries;
+  var flavor;
+  i = 0;
+  while (true) {
+    if (flavors[i].language.name == "en") {
+      flavor = flavors[i].flavor_text;
+      break;
+    }
+    i++;
+  }
+  var flavorElement = document.createElement("p");
+  flavorElement.innerHTML = flavor;
+  dataDiv.appendChild(flavorElement);
+
+  entry.appendChild(dataDiv);
+
+  pokedexEntries.appendChild(entry);
+}
+
+function titleCase(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
