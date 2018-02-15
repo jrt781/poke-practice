3fa54e9ff488a05ebb5cb171169a59aaba55d863
Jake Tyler <jrt@byu.edu>
2018-02-14 23:43:12 -0700

    quiz functionality

---
+++ quiz.js
@@ -0,0 +1,88 @@
+$(document).ready(function () {
+
+  $("#startQuizButton").css("display", "none");
+  $("#progressBar").css("display", "block");
+  $("#questionAnswers").empty();
+  makeNameToImageQuestion();
+  
+  $("#nextQuestionButton").click(function(e) {
+    e.preventDefault();
+    $("#quiz").css("display", "none");
+    $("#questionAnswers").empty();
+    $("#progressBar").css("display", "block");
+    $("#questionNumber").html(parseInt($("#questionNumber").html()) + 1);
+    makeNameToImageQuestion();
+  });
+});
+
+function makeNameToImageQuestion() {
+  var randomPokemonId = Math.floor((Math.random() * 802) + 1);
+  var pokemonUrl = "https://pokeapi.co/api/v2/pokemon/" + randomPokemonId + "/";
+  var pokemonImageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + randomPokemonId + ".png";
+  $.ajax({
+    url : pokemonUrl,
+    dataType : "json",
+    success : function(pokemonJson) {
+      console.log(pokemonJson);
+      $("#questionText").html("Which of the following Pokémon is " + titleCase(pokemonJson.name) + "?");
+      $("#questionAnswers").append("<div class='four-column correct' onclick='checkAnswer(this, true)'><img src='" + pokemonImageUrl + "' /></div>");
+      var numberAnswersSoFar = $("#questionAnswers").children().length;
+      var percent = 100*numberAnswersSoFar/(4-1);
+      $("#loadedBar").css("width", percent + '%');
+      makeNameToImageIncorrectAnswer();
+    }
+  });
+}
+
+function makeNameToImageIncorrectAnswer() {
+  if ($("#questionAnswers").children().length < 4) {
+    var randomPokemonId = Math.floor((Math.random() * 802) + 1);
+    var pokemonUrl = "https://pokeapi.co/api/v2/pokemon/" + randomPokemonId + "/";
+    var pokemonImageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + randomPokemonId + ".png";
+    $.ajax({
+      url : pokemonUrl,
+      dataType : "json",
+      success : function(pokemonJson) {
+        console.log(pokemonJson);
+        var answerHTML = "<div class='four-column incorrect' onclick='checkAnswer(this, false)'><img src='" + pokemonImageUrl + "' /></div>"
+        $("#questionAnswers").append(answerHTML);
+        var numberAnswersSoFar = $("#questionAnswers").children().length;
+        var percent = 100*numberAnswersSoFar/(4-1);
+        $("#loadedBar").css("width", percent + '%');
+        makeNameToImageIncorrectAnswer();
+      }
+    });
+  } else {
+    var n = Math.floor(Math.random() * 4);
+    while (n > 0) {
+      $('.correct').each(function() { $(this).insertAfter($(this).next()); });
+      n--;
+    }
+    $("#progressBar").css("display", "none"); 
+    $("#loadedBar").css("width", "1%");
+    $("#quiz").css("display", "block");
+    $("#nextQuestionButton").css("display", "none");
+  }
+}
+
+function checkAnswer(element, correct) {
+  if ($("#complete").html() == "--") {
+    $("#score").html("0");
+    $("#complete").html("0");      
+  }
+  if (parseInt($("#complete").html()) < parseInt($("#questionNumber").html())) {
+    $(".four-column").css("box-shadow", "0px 0px black");
+    $(".correct").css("box-shadow", "0px 0px 18px 5px green");
+    if (correct) {
+      $("#score").html(parseInt($("#score").html()) + 1);
+    } else {
+      element.style.boxShadow = "0px 0px 18px 5px red";
+    }
+    $("#complete").html(parseInt($("#complete").html()) + 1);
+    $("#nextQuestionButton").css("display", "inline");
+  }
+}
+
+function titleCase(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
